842a2d0f721c757e8e7edc9a3123695f6cea9db8
Asqar <asqar@mail.ru>
2022-08-12 21:22:44 +0400

    create php index

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title> Online shop </title>
+</head>
+
+<body>
+<!-- Main container starts here-->
+<div class="main_wrapper">
+
+    <div class="header_logo">
+        This is header logo
+    </div>
+
+    <div class="content_wrapper">
+        This is content
+
+    </div> <!-- /.content_wrapper-->
+
+    <div id="footer">
+        This is footer
+    </div>
+</div> <!-- /.main_wrapper -->
+
+<!-- End Main container here-->
+
+</body>
+
+
+</html>
