d5741476725cf3ec075999e052eea64ce9f3f08d
Asqar <asqar@mail.ru>
2022-08-13 20:37:31 +0400

    Adding a Menu Bar for the Website

--- index.php
+++ index.php
@@ -41,6 +41,16 @@
 
 
     </div> <!-- /.header_wrapper-->
+    <div class="menubar">
+        <ul id="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="all_products.php">All Products</a></li>
+            <li><a href="customer/my_account.php">My Account</a></li>
+            <li><a href="cart.php">Shopping Cart</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+
+    </div>
 
     <div class="content_wrapper">
         This is content
