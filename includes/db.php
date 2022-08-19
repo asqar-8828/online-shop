f6613d3117dcb4c5583ab8033c8b09c08fe3e76d
Asqar <asqar@mail.ru>
2022-08-19 18:07:47 +0400

    Creating MySQL Database and Dynamic Categories of Left Sidebar with PHP MySQLi

---
+++ includes/db.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost","root","","ecommerce_cms_tutorial");
+
+if(mysqli_connect_errno()) {
+    echo "MySQL ga ulanishda xato berdi: " . mysqli_connect_error();
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+include ("includes/db.php");
+
+?>
+
+
 <html>
 <head>
     <title> Online shop </title>
@@ -56,16 +62,32 @@
         <div id="sidebar">
             <div id="sidebar_title">Categories</div>
             <ul id="cats">
-                <li> <a href="index.php">Laptop</a></li>
-                <li> <a href="index.php">Cameras</a></li>
-                <li> <a href="index.php">Mobiles</a></li>
+
+                <?php
+                $get_cats = "select * from categories";
+                $run_cats = mysqli_query($con, $get_cats);
+                while ($row_cats = mysqli_fetch_array($run_cats)) {
+                    $cat_id = $row_cats['cat_id'];
+
+                    $cat_title = $row_cats['cat_title'];
+
+                    echo "<li> <a href='index.php?cat=$cat_id'>$cat_title</a></li>";
+                }
+                ?>
             </ul>
 
             <div id="sidebar_title">Brands</div>
             <ul id="cats">
-                <li> <a href="index.php">HP</a></li>
-                <li> <a href="index.php">Dell</a></li>
-                <li> <a href="index.php">LG</a></li>
+                <?php
+                $get_brands = "select * from brands";
+                $run_brands = mysqli_query($con, $get_brands);
+                while ($row_brands = mysqli_fetch_array($run_brands)) {
+                    $brand_id = $row_brands['brand_id'];
+                    $brand_title = $row_brands['brand_title'];
+                    echo "<li> <a href='index.php?cat=$brand_id'>$brand_title</a></li>";
+                }
+                ?>
+                
             </ul>
 
         </div>
