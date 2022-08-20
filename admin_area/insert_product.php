0827fddbe97ca7d9168c1d6e65bc7a70bff077ec
Asqar <asqar@mail.ru>
2022-08-20 14:34:38 +0400

    Creating Database Table for Insert Product Page

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -88,3 +88,30 @@ include ('includes/db.php')
 
 
 </html>
+
+<?php
+
+if(isset($_POST['insert_post'])){
+    $product_title = $_POST['product_title'];
+    $product_cat = $_POST['product_cat'];
+    $product_brand = $_POST['product_brand'];
+    $product_price = $_POST['product_price'];
+    $product_desc = trim(mysqli_real_escape_string($con,$_POST['product_desc']));
+    $product_keywords = $_POST['product_keywords'];
+
+    //Getting image from the field
+    $product_image = $_FILES['product_image']['name'];
+    $product_image_tmp = $_FILES['product_image']['tmp_name'];
+
+    move_uploaded_file($product_image_tmp,"product_images/$product_image");
+    $insert_product = " insert into products (product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) 
+    values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords') ";
+
+    $insert_pro = mysqli_query($con,$insert_product);
+
+    if($insert_pro) {
+        echo "<script>alert('Mahsulotlat qushildi')</script>";
+        echo "<script>window.open('index.php?insert_product','_self')</script>";
+    }
+}
+?>
