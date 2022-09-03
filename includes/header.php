3b49bf11d428170dc985bc10bf882afe3ba5ed12
Asqar <asqar@mail.ru>
2022-09-03 09:25:51 +0400

    Creating Profile Drop Down Menu

--- includes/header.php
+++ includes/header.php
@@ -49,12 +49,42 @@ include ("includes/db.php");
             </ul>
 
         </div>
+        <?php if(!isset($_SESSION['user_id'])) { ?>
 
         <div class="register_login">
             <div class="login" > <a href="index.php?action=login">Login </a></div>
+
             &nbsp;&nbsp;
             <div class="register"> <a href="register.php">Register</a> </div>
         </div>
+        <?php } else { ?>
+            <?php
+            $select_user = mysqli_query($con, "select * from users where id= '$_SESSION[user_id]'");
+            $data_user = mysqli_fetch_array($select_user);
+            ?>
+        <div id="profile">
+            <ul>
+                <li class="dropdown_header">
+                    <a>
+                        <?php if ($data_user['image'] != '') { ?>
+                            <span><img src="customer/customer_images/<?php echo $data_user['image'];?>"></span>
+
+                        <?php } else { ?>
+                        <span><img src="images/profile-icon.png"></span>
+
+                        <?php }; ?>
+                    </a>
+                    <uL class="dropdown_menu_header">
+                        <li><a href="customer/my_account.php">Account Setting</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+
+                    </uL>
+
+                </li>
+            </ul>
+
+        </div>
+        <?php } ?>
 
 
     </div> <!-- /.header_wrapper-->
