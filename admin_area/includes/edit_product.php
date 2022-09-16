dfb137a03397f0a6539e2561b2977262a1944c97
Asqar <asqar@mail.ru>
2022-09-16 19:36:31 +0400

    Editing Product Process

--- admin_area/includes/edit_product.php
+++ admin_area/includes/edit_product.php
@@ -96,7 +96,7 @@ $fetch_edit = mysqli_fetch_array($edit_product);
 
             <tr>
                 <td></td>
-                <td colspan="7"><input type="submit" name="insert_post" value="Add Product" /></td>
+                <td colspan="7"><input type="submit" name="edit_product" value="Save" /></td>
 
             </tr>
         </table>
@@ -106,27 +106,34 @@ $fetch_edit = mysqli_fetch_array($edit_product);
 
 <?php
 
-if(isset($_POST['insert_post'])){
-    $product_title = $_POST['product_title'];
+if(isset($_POST['edit_product'])){
+    $product_title = trim(mysqli_real_escape_string($con, $_POST['product_title']));
     $product_cat = $_POST['product_cat'];
     $product_brand = $_POST['product_brand'];
     $product_price = $_POST['product_price'];
     $product_desc = trim(mysqli_real_escape_string($con,$_POST['product_desc']));
     $product_keywords = $_POST['product_keywords'];
+    $date = date("F d, Y");
 
     //Getting image from the field
     $product_image = $_FILES['product_image']['name'];
     $product_image_tmp = $_FILES['product_image']['tmp_name'];
 
-    move_uploaded_file($product_image_tmp,"product_images/$product_image");
-    $insert_product = " insert into products (product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) 
-    values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords') ";
+    if (!empty($_FILES['product_image']['name'])){
+        if (move_uploaded_file($product_image_tmp,"product_images/$product_image")) {
+            $update_product = mysqli_query($con, "update products set product_cat = '$product_cat', product_brand='$product_brand', product_title = '$product_title', product_price= '$product_price', product_desc = '$product_desc', product_image = '$product_image', product_keywords = '$product_keywords', date='$date' where product_id = '$_GET[product_id]'");
+        }
 
-    $insert_pro = mysqli_query($con,$insert_product);
 
-    if($insert_pro) {
-        echo "<script>alert('Mahsulotlat qushildi')</script>";
-        //echo "<script>window.open('index.php?insert_product','_self')</script>";
+    } else {
+        $update_product = mysqli_query($con, "update products set product_cat = '$product_cat', product_brand='$product_brand', product_title = '$product_title', product_price= '$product_price', product_desc = '$product_desc',  product_keywords = '$product_keywords', date='$date' where product_id = '$_GET[product_id]'");
     }
+
+    if ($update_product) {
+        echo "<script>alert('Product was updated successfully! ')</script>";
+        echo "<script>window.open(window.location.href,'_self')</script>";
+    }
+
+
 }
 ?>
